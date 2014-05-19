de191cd3c1730a1a2933625a3bab883c116ffac9
gregmcguirk <greg@paynearme.com>
2014-05-19 14:55:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZurbDemo::Application.config.secret_token = '73dbbdc71f8ffbe031a2a0a79e47ec4508349f3c83e8cc00e60e4fb9e60bf789612d69b58ec0f34f4d2077ef0d16b58d5f59bdbf893444844a115dca2dd52a25'
